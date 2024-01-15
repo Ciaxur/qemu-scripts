0fc69fa51202035d424a295edfb12c5761067195
Omar Omar <oomar@umich.edu>
2024-01-15 08:10:14 -0800

    Add get iommu script + add docs for qemu options

--- run-disk.sh
+++ run-disk.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 DISK_PATH=$1
+BIOS_PATH="/usr/share/ovmf/x64/OVMF.fd"
+IOMMU_GPU_GROUP_ID="0c:00.0"
 
 if [[ "$DISK_PATH" = "" ]] || [[ ! -e $DISK_PATH ]]; then
   echo "Disk path $DISK_PATH not found"
@@ -12,16 +14,44 @@ fi
 # SMP stands for Symmetric Multiprocessing. Essentially Virtual CPU cores.
 #
 # -cpu host indicates to qemu to use the best emulation compatibility for the host's CPU.
+
+# In order argument description:
+#   - Enable KVM virtualization.
+#   - Mount raw device.
+#   - Mount BIOS image.
+#   - Enable boot menu.
+#   - Allocate 8GiB of mem to VM.
+#   - Specify the machine time and KVM acceleration.
+#   - Specify 6 vcores.
+#   - Use the host CPU model.
+#   - Set keyboard layout to US.
+#   - Use VNC display :5901
+#   - Specify the BIOS image.
+#   - Configure a network interface.
+#   - Port forward host:5555 <-> guest:22.
+#   - Disable VGA.
 qemu-system-x86_64 \
   -enable-kvm \
   -drive file="$DISK_PATH",format=raw \
+  -drive if=pflash,format=raw,readonly=on,file="$BIOS_PATH" \
+  -boot menu=on \
   -m 8G \
   -machine type=q35,accel=kvm \
   -smp 6 \
   -cpu host \
   -k en-us \
   -display vnc=:1 \
-  -bios /usr/share/ovmf/x64/OVMF.fd \
-  -vga virtio
+  -bios "$BIOS_PATH" \
+  -net nic \
+  -net user,hostfwd=tcp::5555-:22,hostname=guestOS \
+  -vga none
 
-
+# Removed options.
+#   - Use virtio for VGA.
+#   - Forward serial output to stdio.
+#   - Passthrough host GPU device to VM. multifunction exposes all GPU functions to the VM.
+#   - Disable virtualized graphics.
+  #-vga virtio \
+  #-serial stdio \
+  #-device vfio-pci,host=$IOMMU_GPU_GROUP_ID,bus=pcie.0
+  #-nographic
